1be6b9fc947050611e15c5cfb89fa57ee47d8c35
horyd <prods3@gmail.com>
2014-05-30 21:34:21 +1000

    api ready

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require('express')
+, mongoose = require('mongoose')
 , app = express();
+
+mongoose.connect('mongodb://theCoderFactory:angularjs@ds051848.mongolab.com:51848/angularjs_class');
+
+var Schema = mongoose.Schema
+, ObjectId = Schema.ObjectId
+, itemSchema = new Schema({
+	category: String,
+	name: String,
+	price: Number
+});
+var Item = mongoose.model('items',itemSchema);
 
 app.use(express.bodyParser());
 app.use(express.cookieParser());
@@ -7,6 +19,12 @@ app.use(express.static(__dirname));
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(app.router);
+
+app.get('/api/items', function (req,res) {
+	Item.find().exec(function (err, items){
+		res.json(items);
+	});
+})
 
 app.get('*',function (req,res) {
 	res.sendfile('index.html');
